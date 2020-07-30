05477162ec925e2b9bc2d507f6fa3d52dcf8282a
jyothishkjames <jyothishlp@gmail.com>
2020-07-30 19:58:47 +0200

    feat: adding function plot_spectrogram

--- data/process_data.py
+++ data/process_data.py
@@ -13,3 +13,15 @@ def load_images():
 
     y_train = train_labels.values
     y_val = val_labels.values
+
+
+def plot_spectrogram():
+    plt.figure(0, figsize=(12, 12))
+    for i in range(1, 4):
+        plt.subplot(1, 3, i)
+        img = np.squeeze(x_train[np.random.randint(0, x_train.shape[0])])
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(img)
+
+    plt.imshow(np.squeeze(x_train[3]), cmap="gray");
