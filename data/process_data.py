824bb8ca1939f67d22acd225ce3129f33680d7ae
jyothishkjames <jyothishlp@gmail.com>
2020-08-06 11:23:05 +0200

    feat: adding main function

--- data/process_data.py
+++ data/process_data.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 def load_images():
-    train_images = pd.read_csv('train/images.csv', header=None)
-    train_labels = pd.read_csv('train/labels.csv', header=None)
+    train_images = pd.read_csv('dataset/train/images.csv', header=None)
+    train_labels = pd.read_csv('dataset/train/labels.csv', header=None)
 
-    val_images = pd.read_csv('validation/images.csv', header=None)
-    val_labels = pd.read_csv('validation/labels.csv', header=None)
+    val_images = pd.read_csv('dataset/validation/images.csv', header=None)
+    val_labels = pd.read_csv('dataset/validation/labels.csv', header=None)
 
     x_train = train_images.values.reshape(3200, 64, 128, 1)
     x_val = val_images.values.reshape(800, 64, 128, 1)
@@ -15,8 +18,10 @@ def load_images():
     y_train = train_labels.values
     y_val = val_labels.values
 
+    return x_train, x_val, y_train, y_val
 
-def plot_spectrogram():
+
+def plot_spectrogram(x_train, ):
     plt.figure(0, figsize=(12, 12))
     for i in range(1, 4):
         plt.subplot(1, 3, i)
@@ -28,9 +33,24 @@ def plot_spectrogram():
     plt.imshow(np.squeeze(x_train[3]), cmap="gray")
 
 
-def create_data_generators():
+def create_data_generators(x_train, x_val):
     datagen_train = ImageDataGenerator(horizontal_flip=True)
     datagen_train.fit(x_train)
 
     datagen_val = ImageDataGenerator(horizontal_flip=True)
     datagen_val.fit(x_val)
+
+    return datagen_train, datagen_val
+
+
+def main():
+
+    print("Loading data...")
+
+    load_images()
+
+    print("Saving data as pickle file...")
+
+
+if __name__ == '__main__':
+    main()
